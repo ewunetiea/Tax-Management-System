001d03169e80bf5da7a81e0e1fc31592b841890a
AbeshuK <abeshuk@awashbank.com>
2025-08-26 11:49:41 +0300

    Some thing is added

--- Backend/TaxManagmentSystem/src/main/java/com/afr/fms/Security/jwt/AuthTokenFilter.java
+++ Backend/TaxManagmentSystem/src/main/java/com/afr/fms/Security/jwt/AuthTokenFilter.java
@@ -1,30 +1,24 @@
 package com.afr.fms.Security.jwt;
 
 import java.io.IOException;
-import java.util.Set;
-
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.filter.OncePerRequestFilter;
-
 import com.afr.fms.Admin.Entity.User;
 import com.afr.fms.Admin.Mapper.UserMapper;
 import com.afr.fms.Admin.Mapper.UserTrackerMapper;
 import com.afr.fms.Common.Permission.Service.FunctionalitiesService;
 import com.afr.fms.Security.UserDetailsServiceImpl;
 import com.afr.fms.Security.UserSecurity.mapper.RefreshTokenMapper;
-
 import io.jsonwebtoken.UnsupportedJwtException;
 
 public class AuthTokenFilter extends OncePerRequestFilter {
